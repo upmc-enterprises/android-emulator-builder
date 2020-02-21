0be97c5c72e446e401cff0bdc04ec5baaca702ce
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2020-02-21 14:20:52 -0500

    Change currentDir logic

--- setup-host.sh
+++ setup-host.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-source "$currentDir/host/install-android-command-line-tools.sh"
-source "$currentDir/host/install-java.sh"
-source "$currentDir/host/create-paths.sh"
-source "$currentDir/host/install-paths.sh"
-source "$currentDir/host/install-android-sdk.sh"
+currentDirController="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source "$currentDirController/host/install-android-command-line-tools.sh"
+source "$currentDirController/host/install-java.sh"
+source "$currentDirController/host/create-paths.sh"
+source "$currentDirController/host/install-paths.sh"
+source "$currentDirController/host/install-android-sdk.sh"
