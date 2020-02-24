c95d6e57402e49364fd0fe3bf5367d26f3aef04e
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2020-02-24 09:38:04 -0500

    Add Keyboard APK

--- emulator/start-emulator.sh
+++ emulator/start-emulator.sh
@@ -4,5 +4,5 @@ currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$currentDir/../config.sh"
 
 # Start the emulator and wait for the boot to finish
-$installDestination/emulator/emulator -avd "$emulatorDeviceName" -wipe-data &
+$installDestination/emulator/emulator -avd "$emulatorDeviceName" -no-audio -wipe-data &
 adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done;'
