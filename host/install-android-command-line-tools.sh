1fcaeb505be799d2b0f9ac84ac4d69018e8b6bbb
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2020-03-03 11:03:59 -0500

    Move correct folder

--- host/install-android-command-line-tools.sh
+++ host/install-android-command-line-tools.sh
@@ -19,7 +19,7 @@ unzip -q android.zip -d ./android-temp
 echo "Extraction complete"
 
 mkdir -p "$installDestination/cmdline-tools/latest"
-mv ./android-temp/* "$installDestination/cmdline-tools/latest"
+mv ./android-temp/tools/* "$installDestination/cmdline-tools/latest"
 rm -rf ./android-temp
 rm android.zip
 
